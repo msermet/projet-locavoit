ab1b3f25cfa59fad42689922f001661f5c6a22a4
Maxime Sermet <maximesermet075@gmail.com>
2024-03-12 11:52:06 +0100

    locavoit

--- index.php
+++ index.php
@@ -48,8 +48,8 @@
                 <p>Vous permettant de louer un véhicule chez nous en toute sérénité !</p>
             </div>
             <div class="row pt-5 align-items-center">
-                <div class="col-lg-6 col-xl-6">
-                    <img class="d-lg-block my-auto mx-auto rounded-5" src="./assets/images/206+.png" alt="206 +" />
+                <div class="col-lg-6 col-xl-6 text-center">
+                    <img class="d-lg-block my-auto mx-auto rounded-5 img-fluid mb-4" src="./assets/images/206+.png" alt="206 +" />
                 </div>
                 <div class="col-lg-6 col-xl-6">
                     <div class="accordion" id="accordionExample">
@@ -110,7 +110,56 @@
                             <p>Véhicule haut de gamme</p>
                             <h1 class="text-success fw-bold">70 - 300 € / jour</h1>
                             <p class="card-text d-lg-block d-none">Lorem ipsum dolor sit amet. Corporis dolorum explicabo impedit ipsa ipsum magni pariatur quas, ratione voluptates. Libero minima molestiae nesciunt non quaerat quisquam repudiandae rerum vel vero.</p>
-                            <button type="button" class="btn btn-outline-success">Achetez maintenant</button>
+                            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+                                Voir les véhicules disponibles
+                            </button>
+                            <div class="translate-middle badge rounded-pill bg-danger">4
+                                <div class="spinner-border spinner-border-sm ms-1" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
+                            </div>
+
+
+                            <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel" style="width: 40rem">
+                                <div class="offcanvas-header">
+                                    <h5 class="offcanvas-title" id="staticBackdropLabel">Edition confort</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body mx-auto">
+                                    <div class="card mb-5 border-3 border-dark" style="width: 35rem;">
+                                        <img src="./assets/images/audi-tt-rs.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Audi TT RS Coupé</h5>
+                                            <p class="card-text">L’Audi TT RS va vous couper le souffle. Avec son bouclier aux prises d’air plus imposantes, ses ouïes latérales à l’arrière et son aileron redessiné, l’Audi TT RS semble vouloir s’affranchir des lois de l’aérodynamique. Le tout s’accompagne du mythique 5 cylindres TFSI qui saura vous ravir par ses vocalises inimitables. Laissez-vous envoûter par cette icône au sommet.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                    <div class="card mb-5 border-3 border-dark" style="width: 35rem;">
+                                        <img src="./assets/images/jeep-grand-cherokee.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Jeep Gran Cherokee</h5>
+                                            <p class="card-text">Grand Cherokee : l’excellence à son plus haut niveau. Luxe, raffinement et technologie d’avant-garde. A l’intérieur, les inserts en bois, le cuir Palermo de haute qualité et le ciel de toit premium créent une ambiance exclusive. Le passager avant dispose d’un écran tactile de 10’’. Confort, sécurité et sérénité sont garantis grâce à la caméra 360°, à l’assistance à la conduite semi-autonome et au système de vision nocturne qui détecte piétons, cyclistes et animaux.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                    <div class="card mb-5 border-3 border-dark" style="width: 35rem;">
+                                        <img src="./assets/images/bmw-serie4-gran-coupe.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">BMW Série 4 Gran Coupé</h5>
+                                            <p class="card-text">Design expressif, tenue de route sportive et espace généreux au quotidien : la BMW Série 4 Gran Coupé est le coupé sportif de référence et fascine à tous points de vue. Avec son confort de conduite élevé et un espace généreux avec cinq sièges et un grand compartiment à bagages, cet élégant Gran Coupé 5 portes est le compagnon parfait pour toutes les occasions. </p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                    <div class="card mb-5 border-3 border-dark" style="width: 35rem;">
+                                        <img src="./assets/images/audi-q3.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Audi Q3</h5>
+                                            <p class="card-text">Une ligne de toit plongeante, une allure vigoureuse, une calandre Singleframe en nid d’abeilles et une ligne d’épaule basse... voici l’Audi Q3 Sportback, le premier SUV Coupé Compact Audi. Sous une carrosserie musclée, ses motorisations puissantes lui font gagner en dynamisme. A bord, l’expérience de conduite intègre des fonctionnalités plus intuitives que jamais et résolument « 2.0 ». Idéale pour le quotidien mais avec une touche de singularité, il est impossible pour vous de rester indifférent.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -148,7 +197,7 @@
         <div class="container text-center text-black">
             <h1 class="text-center">Organigramme de notre société !</h1>
             <p class="text-center fs-5 fw-lighter">Voici tous les membres prêts à vous accueillir</p>
-            <img src="./assets/images/organigramme.png" class="w-100" alt="">
+            <img src="./assets/images/organigramme.png" class="img-fluid" alt="">
         </div>
     </section>
     <div class="border-top border-3"></div>
@@ -305,31 +354,31 @@
                 <div class="col-6 col-md-2">
                     <h5>Redirection</h5>
                     <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="#presentation" class="nav-link p-0 text-body-secondary">Présentation</a></li>
-                        <li class="nav-item mb-2"><a href="#activites" class="nav-link p-0 text-body-secondary">Activités</a></li>
-                        <li class="nav-item mb-2"><a href="#tarifs" class="nav-link p-0 text-body-secondary">Tarifs</a></li>
-                        <li class="nav-item mb-2"><a href="#organigramme" class="nav-link p-0 text-body-secondary">Organigramme</a></li>
-                        <li class="nav-item mb-2"><a href="#fichesdeposte" class="nav-link p-0 text-body-secondary">Fiches de poste</a></li>
+                        <li class="nav-item mb-2"><a href="#presentation" class="nav-link p-0 text-body-secondary"><i class="bi bi-arrow-right-circle-fill me-2"></i>Présentation</a></li>
+                        <li class="nav-item mb-2"><a href="#activites" class="nav-link p-0 text-body-secondary"><i class="bi bi-arrow-right-circle-fill me-2"></i>Activités</a></li>
+                        <li class="nav-item mb-2"><a href="#tarifs" class="nav-link p-0 text-body-secondary"><i class="bi bi-arrow-right-circle-fill me-2"></i>Tarifs</a></li>
+                        <li class="nav-item mb-2"><a href="#organigramme" class="nav-link p-0 text-body-secondary"><i class="bi bi-arrow-right-circle-fill me-2"></i>Organigramme</a></li>
+                        <li class="nav-item mb-2"><a href="#fichesdeposte" class="nav-link p-0 text-body-secondary"><i class="bi bi-arrow-right-circle-fill me-2"></i>Fiches de poste</a></li>
                     </ul>
                 </div>
                 <div class="col-6 col-md-2">
                     <h5>Localisation</h5>
                     <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a class="text-body-secondary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="https://maps.app.goo.gl/XTaGztzZpoRb5f7R8">3 Rue Gustave Courbet 70150 Marnay</a></li>
-                        <li class="nav-item mb-2"><a class="nav-link p-0 text-body-secondary">Pays : France</a></li>
+                        <li class="nav-item mb-2"><a class="text-body-secondary link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="https://maps.app.goo.gl/XTaGztzZpoRb5f7R8"><i class="bi bi-geo-fill me-2"></i>3 Rue Gustave Courbet 70150 Marnay</a></li>
+                        <li class="nav-item mb-2"><a class="nav-link p-0 text-body-secondary"><i class="bi bi-geo-alt-fill me-2"></i>France</a></li>
 
                     </ul>
                 </div>
                 <div class="col-6 col-md-2">
                     <h5>Contacts</h5>
                     <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a class="nav-link p-0 text-body-secondary">Téléphone : 07 68 72 58 23</a></li>
-                        <li class="nav-item mb-2"><a class="nav-link p-0 text-body-secondary">E-mail : locavoit@gmail.com</a></li>
+                        <li class="nav-item mb-2"><a class="nav-link p-0 text-body-secondary"><i class="bi bi-telephone-outbound-fill me-2"></i>07 68 72 58 23</a></li>
+                        <li class="nav-item mb-2"><a class="nav-link p-0 text-body-secondary"><i class="bi bi-envelope-at-fill me-2"></i>locavoit@gmail.com</a></li>
                     </ul>
                 </div>
                 <div class="col-md-5 offset-md-1">
-                    <h5>Si vous avez une question à nous poser :</h5>
-                    <p class="text-body-secondary">Remplissez le formulaire de contact</p>
+                    <h5>Une question ?<i class="bi bi-person-raised-hand ms-2"></i></h5>
+                    <p class="text-body-secondary">Remplissez le formulaire de contact :</p>
                     <div class="ms-2">
                         <a href="/_partials/contact.php"><button class="btn btn-success" type="button">Cliquez ici</button></a>
                     </div>
@@ -340,3 +389,5 @@
 <script src="./assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+<!-- uiverse.io = site cool -->
